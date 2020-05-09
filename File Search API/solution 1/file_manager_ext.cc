74d40aaf2f65e90711bc41d0539f8112950725f2
Kishore Debnath <kishoredbn@engineer.com>
2020-05-09 13:10:07 -0400

    Solution: Edit: moving struct definition out of header file.

--- File Search API/solution 1/file_manager_ext.cc
+++ File Search API/solution 1/file_manager_ext.cc
@@ -1,6 +1,24 @@
 #include "common.h"
 #include "file_manager_ext.h"
 
+struct FileManagerV2::THash {
+    size_t operator()(const spIFile &file) const {
+        assert(file != nullptr);
+        auto atr = file->GetFileAttributes();
+        return std::hash<std::string>{}(std::get<std::string>((atr[Attributes::Name]).value)) +
+                std::hash<std::string>{}(std::get<std::string>((atr[Attributes::Extension]).value)) +
+                std::hash<uint64_t>{}(std::get<std::uint64_t>((atr[Attributes::Size]).value));
+    }
+};
+
+struct FileManagerV2::TEqual {
+    bool operator()(const spIFile &file1, const spIFile &file2) const {
+        assert(file1 != nullptr);
+        assert(file2 != nullptr);
+        THash th; return th.operator()(file1) == th.operator()(file2);
+    }
+};
+
 // extending FileManager::Search
 auto FileManagerV2::SearchOr(std::vector<std::tuple<SearchBase, FileMetaType>> vsearch) -> std::vector<spIFile> {
     std::vector<spIFile> ret;
--- File Search API/solution 1/file_manager_ext.h
+++ File Search API/solution 1/file_manager_ext.h
@@ -3,22 +3,8 @@
 // extending FileManager to implememnt Search OR and AND
 class FileManagerV2 : public FileManager {
 private:
-    struct THash {
-        size_t operator()(const spIFile &file) const {
-            assert(file != nullptr);
-            auto atr = file->GetFileAttributes();
-            return std::hash<std::string>{}(std::get<std::string>((atr[Attributes::Name]).value)) +
-                   std::hash<std::string>{}(std::get<std::string>((atr[Attributes::Extension]).value)) +
-                   std::hash<uint64_t>{}(std::get<std::uint64_t>((atr[Attributes::Size]).value));
-        }
-    };
-    struct TEqual {
-        bool operator()(const spIFile &file1, const spIFile &file2) const {
-            assert(file1 != nullptr);
-            assert(file2 != nullptr);
-            THash th; return th.operator()(file1) == th.operator()(file2);
-        }
-    };
+    struct THash;
+    struct TEqual;
     
 public:
     virtual auto SearchOr(std::vector<std::tuple<SearchBase, FileMetaType>> vsearch) -> std::vector<spIFile>;
